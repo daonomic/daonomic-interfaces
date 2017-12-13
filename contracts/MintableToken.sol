ea881612757282495b0223d34f58be5719d6f9d0
0v1se <dev@0v1se.com>
2017-12-13 10:33:03 +0300

    MintableToken event

--- contracts/MintableToken.sol
+++ contracts/MintableToken.sol
@@ -5,5 +5,7 @@ import "./Token.sol";
 
 
 contract MintableToken is Token {
+    event Mint(address indexed to, uint256 amount);
+
     function mint(address _to, uint256 _amount) public returns (bool);
 }
