6cda3f2c9a1408925caea2583dba860658f5cd09
0v1se <dev@0v1se.com>
2018-05-23 17:33:19 +0300

    added NotifyingToken

---
+++ contracts/NotifyingToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.11;
+
+
+import './Token.sol';
+
+
+/**
+ * @title ERC223 interface. Notifies receiving contract about incoming tokens.
+ * @dev see https://github.com/ethereum/EIPs/issues/223
+ */
+
+contract NotifyingToken is Token {
+    event Transfer(address indexed from, address indexed to, uint256 value, bytes data);
+
+    function transferAndCall(address _to, uint256 _value, bytes _data) public returns (bool);
+}
